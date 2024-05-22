3ca622e8019890aa50d178d618e81053bacbe25e
Dimassand <168108518+Dimassand@users.noreply.github.com>
2024-05-22 23:46:39 +0700

    Update Todos.vue

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,52 +1,55 @@
 <template>
-    <div>
-      <slot name="input"></slot>
-      <ul>
-        <li v-for="kegiatan in filteredKegiatan" :key="kegiatan.id">
-          <input type="checkbox" :checked="kegiatan.selesai" @change="toggleSelesai(kegiatan)" class="checkbox">
-          <span :class="{ 'selesai': kegiatan.selesai }">{{ kegiatan.nama }}</span>
-          <button @click="batalkanKegiatan(kegiatan.id)" v-if="!kegiatan.selesai" class="button">Hapus</button>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Todos',
-    props: {
-      kegiatanList: Array,
-      filterSelesai: Boolean
+  <div>
+    <slot name="input"></slot>
+    <ul>
+      <li v-for="kegiatan in filteredKegiatan" :key="kegiatan.id">
+        <input type="checkbox" :checked="kegiatan.selesai" @change="toggleSelesai(kegiatan)" class="checkbox">
+        <span :class="{ 'selesai': kegiatan.selesai }">{{ kegiatan.nama }}</span>
+        <button @click="batalkanKegiatan(kegiatan.id)" v-if="!kegiatan.selesai" class="button">Hapus</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  props: {
+    kegiatanList: {
+      type: Array,
+      required: true
     },
-    computed: {
-      filteredKegiatan() {
-        if (this.filterSelesai) {
-          return this.kegiatanList.filter(kegiatan => !kegiatan.selesai);
-        } else {
-          return this.kegiatanList;
-        }
-      }
-    },
-    methods: {
-      toggleSelesai(kegiatan) {
-        this.$emit('toggle-selesai', kegiatan);
-      },
-      batalkanKegiatan(id) {
-        this.$emit('batalkan-kegiatan', id);
-      }
+    filterSelesai: {
+      type: Boolean,
+      required: true
     }
-  };
-  </script>
-  
-  <style scoped>
-  .selesai {
-    text-decoration: line-through;
+  },
+  computed: {
+    filteredKegiatan() {
+      return this.filterSelesai
+        ? this.kegiatanList.filter(kegiatan => !kegiatan.selesai)
+        : this.kegiatanList;
+    }
+  },
+  methods: {
+    toggleSelesai(kegiatan) {
+      this.$emit('toggle-selesai', kegiatan);
+    },
+    batalkanKegiatan(id) {
+      this.$emit('batalkan-kegiatan', id);
+    }
   }
-  .checkbox {
-    margin-right: 5px;
-  }
-  .button {
-    margin-left: auto;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.selesai {
+  text-decoration: line-through;
+}
+.checkbox {
+  margin-right: 5px;
+}
+.button {
+  margin-left: auto;
+}
+</style>
